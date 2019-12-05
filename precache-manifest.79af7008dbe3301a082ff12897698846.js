d80bb418e7d47e32bf5e83513bf9f29037be9f28
ivan <s.ivankorobchuk@gmail.com>
2019-12-05 23:04:47 +0200

    Updates

---
+++ precache-manifest.79af7008dbe3301a082ff12897698846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cf2583da6bf0f47a3b0131914d108b",
+    "url": "/mainTask/index.html"
+  },
+  {
+    "revision": "8c897209f7dd7db5dcdc",
+    "url": "/mainTask/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "0bdcc68681c09b54df9f",
+    "url": "/mainTask/static/css/main.01fc9a42.chunk.css"
+  },
+  {
+    "revision": "8c897209f7dd7db5dcdc",
+    "url": "/mainTask/static/js/2.86fcbcfd.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/mainTask/static/js/2.86fcbcfd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bdcc68681c09b54df9f",
+    "url": "/mainTask/static/js/main.fefb9903.chunk.js"
+  },
+  {
+    "revision": "68d79b0cad766fe0d739",
+    "url": "/mainTask/static/js/runtime-main.e4115c1e.js"
+  }
+]);
